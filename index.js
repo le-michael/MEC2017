092da66de9801cf79d40f789398ec3099d1b7793
le-michael <zerglinglover@gmail.com>
2017-10-21 17:33:58 -0400

    requestify package

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
-
+const requestify = require('requestify');
 
 app.use(express.static('static'));
 
