8ce5b145780b72b7e648fb517c46cc3fbd88e46c
Alexie McDonald <alexiemcdonald304@gmail.com>
2017-10-21 20:09:16 -0400

    Changed showAlert function

--- static/alert.js
+++ static/alert.js
@@ -1,23 +1,21 @@
-(function ($) {
-    $(function () {
-        var $toastContent = $('<span>New Alert!</span>').add($('<button class="btn-flat toast-action" onclick="$(&#39#modal1&#39).modal(&#39open&#39);">Open</button>'));
-        Materialize.toast($toastContent, 10000, 'red');
+function showAlert(jsonfile) {
+    var $toastContent = $('<span>New Alert!</span>').add($('<button class="btn-flat toast-action" onclick="$(&#39#modal1&#39).modal(&#39open&#39);">Open</button>'));
+    Materialize.toast($toastContent, 10000, 'red');
 
     //initialize all modals           
     $('.modal').modal();
     $('.trigger-modal').modal();
-
-    }); // end of document ready
-    })(jQuery);
-
-    $.getJSON( "./Data/testdata.json", function( data ) {
-    var items = [];
-    $.each( data, function( key, val ) {
-    items.push( "<p><b>" + key + "</b>: " +  val + "</p>" );
-    });
- 
-    $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-    }).appendTo( ".modal-content" );
-    });
+    
+    $.getJSON(jsonfile, function( data ) {
+        var items = [];
+        $.each( data, function( key, val ) {
+        items.push( "<p><b>" + key + "</b>: " +  val + "</p>" );
+        });
+     
+        $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+        }).appendTo( ".modal-content" );
+        });
+    }; // end of document ready
+    (jQuery);
